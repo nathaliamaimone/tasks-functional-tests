373e1048ff49548aeea18352175092aa48214aa4
nathaliamaimone <nathalia.pmaimone@gmail.com>
2025-01-26 22:22:39 -0300

    Add remove task scenario

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -88,4 +88,25 @@ public class TasksTest {
 			driver.quit();
 		}
     }
+
+	@Test
+	public void shouldRemoveTaskWithSuccess() throws MalformedURLException {
+		WebDriver driver = accessApplication();
+		try {
+			// Adding Task
+			driver.findElement(By.id("addTodo")).click();
+			driver.findElement(By.id("task")).sendKeys("Teste via selenium");
+			driver.findElement(By.id("dueDate")).sendKeys("10/10/2040");
+			driver.findElement(By.id("saveButton")).click();
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+			
+			//Removing Task
+			driver.findElement(By.linkText("Remove")).click();
+			message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+		} finally {
+			driver.quit();
+		}
+	}
 }
